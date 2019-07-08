4be1671bbc26ed877b20a4129d31d7d72b2f9c45
JesuitTreason <52435499+JesuitTreason@users.noreply.github.com>
2019-07-07 23:27:41 -0500

    Update deployTraeficConsul.sh

--- deployTraeficConsul.sh
+++ deployTraeficConsul.sh
@@ -1,2 +1,19 @@
 docker network create --driver=overlay traefik-public
 docker network create -d overlay --attachable traefik-private
+
+export EMAIL=admin@example.com
+export DOMAIN=sys.example.com
+
+export USERNAME=admin
+export PASSWORD=changethis
+export HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
+echo $HASHED_PASSWORD
+
+export CONSUL_REPLICAS=2
+export CONSUL_REPLICAS=3
+
+export TRAEFIK_REPLICAS=$(docker node ls -q | wc -l)
+
+docker stack deploy -c traefik.yml traefik-consul
+
+docker stack ps traefik-consul
